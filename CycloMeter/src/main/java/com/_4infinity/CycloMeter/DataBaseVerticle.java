a0fb9f2ecaaea6162da4429ab3a22028743c7eb8
Sayrax97 <dusann.jankovic@elfak.rs>
2020-04-25 15:59:08 +0200

    databse vericle set up

--- CycloMeter/src/main/java/com/_4infinity/CycloMeter/DataBaseVerticle.java
+++ CycloMeter/src/main/java/com/_4infinity/CycloMeter/DataBaseVerticle.java
@@ -5,19 +5,33 @@ import io.vertx.core.Future;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.json.JsonObject;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
 
 public class DataBaseVerticle extends AbstractVerticle {
+  private MySQLPool client;
   @Override
   public void start() {
-    JsonObject food=new JsonObject();
-    food.put("1","palacinke");
-    food.put("2","pizza");
-
     EventBus eventBus=vertx.eventBus();
     MessageConsumer<JsonObject> consumer=eventBus.consumer("data.base.mysql");
+    MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+      .setPort(3306)
+      .setHost("localhost")
+      .setDatabase("cyclometer")
+      .setUser("root")
+      .setPassword("");
+      PoolOptions poolOptions = new PoolOptions()
+        .setMaxSize(5);
+      client = MySQLPool.pool(connectOptions, poolOptions);
+
     consumer.handler(message->{
-      if (message.body().getString("method").equals("getFood"))
-          message.reply(food);
+      if (message.body().getString("method").equals("getUser")){
+
+      }
+
+
+
     });
     vertx.createHttpServer().requestHandler(request ->{
       request.response().end("CycloMeter DataBase Verticle");
@@ -30,6 +44,10 @@ public class DataBaseVerticle extends AbstractVerticle {
           System.out.println("CycloMeter  DataBase Verticle failed");
         }
       });
+  }
+  private JsonObject getUser(int id){
+    client.query("SELECT * FROM user");
+    return new JsonObject();
   }
 
   @Override
